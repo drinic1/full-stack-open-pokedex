efba2542a362d7685c80a771998d596f37ab9d19
drinic1 <223826624+drinic1@users.noreply.github.com>
2025-10-23 15:02:40 +0200

    add e2e test

---
+++ e2e-tests/example.spec.js
@@ -0,0 +1,15 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+const { describe } = require("node:test");
+
+describe("Pokedex", () => {
+  test("front page can be opened", async ({ page }) => {
+    await page.goto("");
+    await expect(page.getByText("ivysaur")).toBeVisible();
+    await expect(
+      page.getByText(
+        "Pokémon and Pokémon character names are trademarks of Nintendo."
+      )
+    ).toBeVisible();
+  });
+});
