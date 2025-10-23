f8bb0bc7cfbf1f16b9f5730b3453bb931c1ec8d1
drinic1 <223826624+drinic1@users.noreply.github.com>
2025-10-23 12:29:30 +0200

    fix linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,14 +15,14 @@ module.exports = {
   plugins: ["react", "jest"],
   rules: {
     indent: ["error", 2],
-    "linebreak-style": ["error", "windows"],
-    // quotes: ["error", "single"],
-    // semi: ["error", "never"],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "never"],
     eqeqeq: "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": ["error", "always"],
     "arrow-spacing": ["error", { before: true, after: true }],
-    // "no-console": "error",
+    "no-console": "error",
     "react/prop-types": 0,
   },
 };
