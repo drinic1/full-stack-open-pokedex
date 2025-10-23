7361f804d9fd70c787743bc0152185843f4321d6
drinic1 <223826624+drinic1@users.noreply.github.com>
2025-10-23 11:58:27 +0200

    add hello world workflow

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 // get the port from env variable
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
